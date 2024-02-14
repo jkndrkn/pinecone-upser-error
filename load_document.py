a9fe49ab235be4476abbb97edca98ae43386da76
John David Eriksen <jeriksen@bamboohr.com>
2024-02-14 11:48:12 -0500

    re-create connection before every upsert

--- load_document.py
+++ load_document.py
@@ -1,13 +1,16 @@
 from os import environ
+from time import sleep
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_pinecone import Pinecone as PineconeStore
+from pinecone import Pinecone as PineconeClient
 
 INDEX_NAME = environ["PINECONE_INDEX"]
 TRIALS = 50
 TEXT_PATH = "my_text.txt"
+PINECONE_API_KEY = environ["PINECONE_API_KEY"]
 
 loader = TextLoader(TEXT_PATH)
 documents = loader.load()
@@ -20,4 +23,8 @@ embedder = HuggingFaceEmbeddings(model_name="all-mpnet-base-v2")
 
 for i in range(0, TRIALS):
     print("trial: ", i, flush=True)
-    PineconeStore.from_documents(docs, embedder, index_name=INDEX_NAME)
+
+    # Explicitly re-create the connection prior to every upsert
+    pinecone_client = PineconeClient(api_key=PINECONE_API_KEY)
+    PineconeStore._index = pinecone_client.Index(INDEX_NAME)
+    PineconeStore.from_documents(docs[:1], embedder, index_name=INDEX_NAME)
